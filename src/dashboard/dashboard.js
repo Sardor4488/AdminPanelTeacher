f470b909e057ed427b7e669e7a21e13adf587a02
Sardor4488 <80748620+Sardor4488@users.noreply.github.com>
2021-10-07 02:04:56 +0500

    work

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import DashboardWrapper from "./dashboardWrapper";
 import logo from "./Logo 1.png";
-import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionSummary from "@mui/material/AccordionSummary";
 import adminImg from "./Ellipse 5.png";
 import "bootstrap/dist/css/bootstrap.min.css";
- const Dashboard = (props) => {
+import { Accordion } from "react-bootstrap";
+const Dashboard = (props) => {
   return (
     <DashboardWrapper>
       <div className="dashboard">
@@ -12,21 +13,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
           <div className="text-center py-3">
             <img src={logo} className="" alt="" />
           </div>
-          <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header" 
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+          <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Accordion Item #1</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Accordion Item #2</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         </div>
         <div className="rightSide shadow">
           <header className="w-100 text-center d-flex justify-content-end p-4 align-align-items-center shadow pt-3">
@@ -35,6 +47,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
               <p className="m-0 fw-bold  ">Admin</p>
             </div>
           </header>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
+          <h1>iiiii</h1>
         </div>
         <div className="content">{props.children}</div>
       </div>
--- src/dashboard/dashboardWrapper.js
+++ src/dashboard/dashboardWrapper.js
@@ -2,14 +2,35 @@ import styled from "styled-components";
 const DashboardWrapper = styled.div`
   .dashboard {
     display:flex;
-    height: 100vh;
-  }
+   }
    .sidebar{
      width:300px;
+     height:100vh;
+     position:sticky;
+     top:0;
+     background-color:#222D32;
+    }
+    .accordion-item{
+     background-color:#222D32;
+color:#fff;
+    }
+    .collapsed{
+      background-color:#222D32;
+      color:#fff
+    }
+    .accordion-button:focus{
+      background-color:#2C3B41  !important;
+      
+    }
+    
+    header{
+      position:sticky;
+      top:0;
+      background-color:#F5F5F7;
     }
    .rightSide{
     flex:1;
-
+       
  
     }
     .content{
